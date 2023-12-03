a292e66c5fbc0892fffb36c5d7ab117f7198208b
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-12-03 10:59:09 +0600

    New added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAV34IUaNvXWV7eCiQDQ203S2ouQHMSrQU",
-  authDomain: "practic-project-501b7.firebaseapp.com",
-  databaseURL: "https://practic-project-501b7-default-rtdb.firebaseio.com",
-  projectId: "practic-project-501b7",
-  storageBucket: "practic-project-501b7.appspot.com",
-  messagingSenderId: "999353662256",
-  appId: "1:999353662256:web:3ee301e8e0e666e53d66f5"
+  apiKey: "AIzaSyA0Yijfdgjx9FvyK7FmahRUCZ-M6eIsuV4",
+  authDomain: "easy-shop-1df4d.firebaseapp.com",
+  projectId: "easy-shop-1df4d",
+  storageBucket: "easy-shop-1df4d.appspot.com",
+  messagingSenderId: "769597411111",
+  appId: "1:769597411111:web:f6d305070ebc284634e302"
 };
 
 
